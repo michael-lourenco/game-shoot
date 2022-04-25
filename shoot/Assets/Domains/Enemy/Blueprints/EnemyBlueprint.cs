aa11998640683ef98cd7c63b7e436b606c0e9d6c
Michael Lourenço <kontempler@gmail.com>
2022-04-25 00:46:20 -0300

    Improve enemy behaviours

--- shoot/Assets/Domains/Enemy/Blueprints/EnemyBlueprint.cs
+++ shoot/Assets/Domains/Enemy/Blueprints/EnemyBlueprint.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Enemy", menuName = "ScriptableObjects/Enemies", order = 1)]
 public class EnemyBlueprint : ScriptableObject
 {
-    public string name;
+    public string enemyName;
     public int hp;
 
 }
